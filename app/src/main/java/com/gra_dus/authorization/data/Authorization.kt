169ee699a9887585c98af7626f8f6088702ea3e0
Gra-Dus <65005478+Gra-Dus@users.noreply.github.com>
2021-06-18 17:58:01 +0300

    Первый таск завершен

--- app/src/main/java/com/gra_dus/authorization/data/Authorization.kt
+++ app/src/main/java/com/gra_dus/authorization/data/Authorization.kt
@@ -1,4 +1,5 @@
 package com.gra_dus.authorization.data
 
 interface Authorization {
+    suspend fun checkAuth(login: String, password: String):Boolean
 }
--- app/src/main/java/com/gra_dus/authorization/model/WelcomeViewModel.kt
+++ app/src/main/java/com/gra_dus/authorization/model/WelcomeViewModel.kt
@@ -3,6 +3,6 @@ package com.gra_dus.authorization.model
 import androidx.lifecycle.ViewModel
 
 class WelcomeViewModel : ViewModel() {
-    val text:String = ""
+    val text:String = "Welcome samurai"
     // TODO: Implement the ViewModel
 }
